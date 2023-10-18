b7d6989b613d72656c727f582cee628ac4282eef
SkorobagatiyMykola <nick1980457@gmail.com>
2023-10-18 13:59:40 +0300

    Initial commit

---
+++ src/main/java/ua/skorobahatyi/my_tests/CompareMap.java
@@ -0,0 +1,13 @@
+package ua.skorobahatyi.my_tests;
+
+import java.util.*;
+
+public class CompareMap {
+    public static void main(String[] args) {
+        Map<Integer,String> map1 = new Hashtable<>();
+        Map<Integer,String> map2 = new HashMap<>();
+        Map<Integer,String> map3 = new LinkedHashMap<>();
+        Map<Integer,String> map4 = new TreeMap<>();
+
+    }
+}
